9e1158f75e0d2dd8c90762ea20113e794435208a
sheng855174 <sheng855174@gmail.com>
2020-03-15 22:07:21 +0800

    init

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const http = require("http");
+//引用'http'模組
+const http = require('http');
 
-const hostname = "127.0.0.1";
+//設定server網址，因為在本機端測試，所以輸入127.0.0.1
+//const hostname = '127.0.0.1'  //上傳至伺服器需拿掉
+
+//port 號會由 Heroku 給予，因此不再自行指定
 const port = process.env.PORT || 3000;
 
+//新增一個server並指定他的頁面資訊，內容為'Hello World!'
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//監聽得到的 port 號開啟
+server.listen(port, () => console.log(`Listening on ${port}`));
