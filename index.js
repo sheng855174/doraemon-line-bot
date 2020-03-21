1b4059b62a0d8f9248b56d0d1a1c255f250b882b
sheng855174 <sheng855174@gmail.com>
2020-03-22 04:03:20 +0800

    init

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ const linebot = require('linebot');
 const express = require('express');
 
 const bot = linebot({
-    channelId: '1601315402',
-    channelSecret: '6af8e930dab9b3a977719185adc48a99',
-    channelAccessToken: 'E8M/Zgv4jKfyH/13Y/qcZ3YosAzkp9IRUFDRN0ZOYvKUVFLBrjqCAQzKruz6y2dvODQ57vf07Y+DlJJ4CZCHzToyhLm8dhPkmcz4aFNwFicKuHnDl2WGeNTA9id2jRVhr4Mhr2RyoP39f+7Wonbl0gdB04t89/1O/w1cDnyilFU='
+	channelId: '1601315402',
+	channelSecret: '6af8e930dab9b3a977719185adc48a99',
+	channelAccessToken: 'E8M/Zgv4jKfyH/13Y/qcZ3YosAzkp9IRUFDRN0ZOYvKUVFLBrjqCAQzKruz6y2dvODQ57vf07Y+DlJJ4CZCHzToyhLm8dhPkmcz4aFNwFicKuHnDl2WGeNTA9id2jRVhr4Mhr2RyoP39f+7Wonbl0gdB04t89/1O/w1cDnyilFU='
 });
 
 
-//這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
-bot.on('message', function(event) {
-    if (event.message.type = 'text') {
-        let msg = event.message.text;
-        //收到文字訊息時，直接把收到的訊息傳回去，這裏是 echo，就是你問什麼就回答什麼，簡單的對話
-        event.reply(msg).then(function(data) {
-            // 傳送訊息成功時，可在此寫程式碼
-            console.log(msg);
-        }).catch(function(error) {
-            // 傳送訊息失敗時，可在此寫程式碼
-            console.log('錯誤產生，錯誤碼：'+error);
-        });
-    }
+
+bot.on('message', function(event) {//這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
+	if (event.message.type = 'text') {//收到文字訊息時，直接把收到的訊息傳回去，這裏是 echo，就是你問什麼就回答什麼，簡單的對話
+		let msg = event.message.text;
+		/*
+		event.reply(msg).then(function(data) {// 傳送訊息成功時，可在此寫程式碼
+			console.log(msg);
+		}).catch(function(error) {// 傳送訊息失敗時，可在此寫程式碼
+			console.log('錯誤產生，錯誤碼：'+error);
+		});
+		*/
+	}
 });
 
 const app = express();
@@ -30,6 +29,6 @@ const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 
 const server = app.listen(process.env.PORT || 80, function() {
-    let port = server.address().port;
-    console.log('目前的port是', port);
+	let port = server.address().port;
+	console.log('目前的port是', port);
 });
