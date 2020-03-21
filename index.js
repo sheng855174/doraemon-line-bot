30334bce35841ab1e3fa30e87019e4f75826320c
sheng855174 <sheng855174@gmail.com>
2020-03-22 03:44:36 +0800

    init

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var player = require("./logger.js");
+// 引用linebot SDK
+var linebot = require('linebot');
 
-const inori = new player('Inori', 16, 'girl', 'pink')
-
-
-//引用'http'模組
-const http = require('http');
-
-//port 號會由 Heroku 給予，因此不再自行指定
-const port = process.env.PORT || 3000;
-
-//新增一個server並指定他的頁面資訊，內容為'Hello World!'
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n' + inori.toString());
+// 用於辨識Line Channel的資訊
+var bot = linebot({
+  channelId: '1601315402',
+  channelSecret: '6af8e930dab9b3a977719185adc48a99',
+  channelAccessToken: 'E8M/Zgv4jKfyH/13Y/qcZ3YosAzkp9IRUFDRN0ZOYvKUVFLBrjqCAQzKruz6y2dvODQ57vf07Y+DlJJ4CZCHzToyhLm8dhPkmcz4aFNwFicKuHnDl2WGeNTA9id2jRVhr4Mhr2RyoP39f+7Wonbl0gdB04t89/1O/w1cDnyilFU='
 });
 
-//監聽得到的 port 號開啟
-server.listen(port, () => console.log(`Listening on ${port}`));
+// 當有人傳送訊息給Bot時
+bot.on('message', function (event) {
+  // event.message.text是使用者傳給bot的訊息
+  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
+  event.reply(event.message.text).then(function (data) {
+    // 當訊息成功回傳後的處理
+  }).catch(function (error) {
+    // 當訊息回傳失敗後的處理
+  });
+});
+
+// Bot所監聽的webhook路徑與port
+bot.listen('/linewebhook', 3000, function () {
+    console.log('[BOT已準備就緒]');
+});
