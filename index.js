1385d23aa7f9900d8a7023a2f764e7d206f162ae
sheng855174 <sheng855174@gmail.com>
2020-03-22 12:01:30 +0800

    init

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const bot = linebot({
 	channelId: '1601315402',
 	channelSecret: '6af8e930dab9b3a977719185adc48a99',
 	channelAccessToken: 'E8M/Zgv4jKfyH/13Y/qcZ3YosAzkp9IRUFDRN0ZOYvKUVFLBrjqCAQzKruz6y2dvODQ57vf07Y+DlJJ4CZCHzToyhLm8dhPkmcz4aFNwFicKuHnDl2WGeNTA9id2jRVhr4Mhr2RyoP39f+7Wonbl0gdB04t89/1O/w1cDnyilFU='
+	verify: true
 });
 
 
 
 bot.on('message', function(event) {//這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
+	let profile = event.source.profile();
 	let userId = event.source.userId;
+	let display_name = profile.displayName;
 	let groupId = event.source.group_id;
 	let type = event.source.type;
-
 	let msg = event.message.text;
 	let txt = msg + "\r\n"
 		+ "userId : " + userId + "\r\n"
 		+ "groupid : " + groupId + "\r\n"
-		+ "type ： " + type + "\r\n";
+		+ "type ： " + type + "\r\n"
+		+ "display_name" + display_name + "\r\n";
 
 	if (event.message.type = 'text') {//收到文字訊息時，直接把收到的訊息傳回去
 		if(userId == "U1062a218fd1dc5967b90f4ab74aa8e15" && msg == "talk"){
