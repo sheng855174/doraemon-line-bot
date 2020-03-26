ffd7fd895ded39be9c8c838ad3cdb4186a242ff4
陳昇昇 <sheng855174@playground.com.tw>
2020-03-26 15:11:44 +0800

    'init'

---
+++ UserProfile.js
@@ -0,0 +1,19 @@
+module.exports = class UserProfile {
+	constructor(userId, type, msg_type, displayName, pictureUrl, message){
+		this.userId = userId;
+		this.type = type;
+		this.msg_type = msg_type;
+		this.displayName = displayName;
+		this.pictureUrl = pictureUrl;
+		this.message = message;
+	}
+	toString() {
+		return "message : " + this.msg + "\r\n"
+		+ "userId : " + this.userId + "\r\n"
+		+ "spaces type ： " + this.type + "\r\n"
+		+ "message type ： " + this.msg_type + "\r\n"
+		+ "displayName : " + this.profile.displayName + "\r\n"
+		+ "pictureUrl : " + this.profile.pictureUrl + "\r\n";
+	}
+};
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const linebot = require('linebot');
 const express = require('express');
-const userProfile = require("./UserProfileAPI.js");
+const UserProfile = require("./UserProfile.js");
 
 const bot = linebot({
 	channelId: '1601315402',
@@ -8,25 +8,21 @@ const bot = linebot({
 	channelAccessToken: 'E8M/Zgv4jKfyH/13Y/qcZ3YosAzkp9IRUFDRN0ZOYvKUVFLBrjqCAQzKruz6y2dvODQ57vf07Y+DlJJ4CZCHzToyhLm8dhPkmcz4aFNwFicKuHnDl2WGeNTA9id2jRVhr4Mhr2RyoP39f+7Wonbl0gdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message', function(event) {//這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
+bot.on('message', function(event) {
 	
 	let userId = event.source.userId;
 	let type = event.source.type;
 	let msg_type = event.message.type;
 	let msg = event.message.text;
 	let date = new Date().toLocaleString('zh-TW', {timeZone: 'Asia/Taipei'});
-	const inori = new userProfile(event);
+	const userProfile = new UserProfile(event);
 	
 	if(msg == 'talk'){
 		event.source.profile().then(function (profile) {
+			userProfile(userId, type, msg_type, profile.displayName, profile.pictureUrl, msg);
 			let txt = msg + "\r\n"
-				+ "userId : " + userId + "\r\n"
-				+ "spaces type ： " + type + "\r\n"
-				+ "message type ： " + msg_type + "\r\n"
-				+ "你的名字是 : " + profile.displayName + "\r\n"
-				+ "你的照片 : " + profile.pictureUrl + "\r\n"
-				+ date + "\r\n"
-				+ inori.toString() + "\r\n";
+				+ 
+				+ date + "\r\n";
 			event.reply(txt);
 		});
 	}
